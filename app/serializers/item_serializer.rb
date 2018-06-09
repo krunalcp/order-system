a213c1bd9c15a5d5a438f511dd3f03583e7a5ad2
KrunaL Parvadiya <krunalcp@gmail.com>
2018-06-09 18:06:23 +0530

    Disable edit and delete when item is used in order

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,7 +1,12 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :quantity
+  attributes :id, :name, :price, :quantity, :item_used
 
   def quantity
   	0
+  end
+
+  def item_used
+   orders = Order.all.pluck(:id)
+   order_items = OrderItem.where(order_id: orders).pluck(:item).include?(object.name)
   end
 end
