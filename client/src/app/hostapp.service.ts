4b450e7f359a1732e4ed9a46ee03653770c1c1c8
KrunaL Parvadiya <krunalcp@gmail.com>
2018-11-14 12:55:02 +0530

    Add path for ctordering-test

--- client/src/app/hostapp.service.ts
+++ client/src/app/hostapp.service.ts
@@ -8,16 +8,19 @@ export class HostappService {
   constructor() { }
 
   public getHost(){
-  	const environment: any = window.location.hostname;
+    const environment: any = window.location.hostname;
 
-  	if (environment == 'localhost'){
-  		return "http://localhost:3000"
-  	}
-  	else if (environment == 'foodup-demo.herokuapp.com'){
+    if (environment == 'localhost'){
+      return "http://localhost:3000"
+    }
+    else if (environment == 'foodup-demo.herokuapp.com'){
       return "https://foodup-demo.herokuapp.com"
-  	}
+    }
     else if (environment == 'ctordering.herokuapp.com'){
       return "https://ctordering.herokuapp.com"
-  	}
+    }
+    else if (environment == 'ctordering-test.herokuapp.com'){
+      return "https://ctordering-test.herokuapp.com"
+    }
   }
 }
