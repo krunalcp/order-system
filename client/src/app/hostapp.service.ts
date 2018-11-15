7e9953956d0860be5a1f28f55e197762e1a94334
KrunaL Parvadiya <krunalcp@gmail.com>
2018-11-15 12:06:17 +0530

    Test site lable added

--- client/src/app/hostapp.service.ts
+++ client/src/app/hostapp.service.ts
@@ -13,14 +13,25 @@ export class HostappService {
     if (environment == 'localhost'){
       return "http://localhost:3000"
     }
-    else if (environment == 'foodup-demo.herokuapp.com'){
-      return "https://foodup-demo.herokuapp.com"
-    }
     else if (environment == 'ctordering.herokuapp.com'){
       return "https://ctordering.herokuapp.com"
     }
     else if (environment == 'ctordering-test.herokuapp.com'){
       return "https://ctordering-test.herokuapp.com"
+    }
+  }
+
+  public isTestSite() {
+    const environment: any = window.location.hostname;
+
+    if (environment == 'localhost'){
+      return true
+    }
+    else if (environment == 'ctordering.herokuapp.com'){
+      return false
+    }
+    else if (environment == 'ctordering-test.herokuapp.com'){
+      return true
     }
   }
 }
