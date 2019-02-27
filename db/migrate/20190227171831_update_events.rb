b653b28547840879d0230abe2bfe97236fb9e43c
KrunaL Parvadiya <krunalcp@gmail.com>
2019-02-27 23:51:04 +0530

    Fix build

--- db/migrate/20190227171831_update_events.rb
+++ db/migrate/20190227171831_update_events.rb
@@ -1,6 +1,6 @@
 class UpdateEvents < ActiveRecord::Migration[5.1]
   def change
     add_column :events, :published_name, :string
-    change_column :events, :tokens, :string, limit: 500
+    # change_column :events, :tokens, :string, limit: 500
   end
 end
